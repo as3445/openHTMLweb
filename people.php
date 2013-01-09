ea2bc87b737489e8d546ef4dec32d061f819d376
Ankur <as3445@drexel.edu>
2013-01-09 15:57:16 -0500

    formatted people page

--- people.php
+++ people.php
@@ -1,28 +1,28 @@
 <?php include("header.php"); ?>
 
 	
-	<div class="row panel radius" id="Andrea" style="margin-left: auto; margin-right: auto; margin-bottom:22px;" >
+	<div class="row panel radius" id="Andrea" style="margin: 0 auto 20px auto;" >
 		<div class="two columns">
 				<img src="/project/images/andrea.jpg">
 		</div>
-
-		<div class="three colums">
-			Andrea Forte
+		<div class="three columns callout panel" style="margin: 0 auto 0 auto; text-align:center;">
+			<h3>Andrea Forte<h3>
 		</div>
-		<div class="seven columns callout panel">
-				Dr. Forte is interested in understanding new forms of information production and sharing that are made possible by participatory media.
-
-	Her. work addresses questions that span human-computer interaction, computer-supported cooperative work, online communities, and learning sciences. She teaches in the areas of social computing and HCI. Dr. Forte’s professional experience includes human-computer interaction design and managing special library collections.
+		<div class="seven columns">
+			Dr. Forte is interested in understanding new forms of information production and sharing that are made possible by participatory media.
+			Her work addresses questions that span human-computer interaction, computer-supported cooperative work, online communities, and learning sciences. She teaches in the areas of social computing and HCI. Dr. Forte’s professional experience includes human-computer interaction design and managing special library collections.
 		</div>
-		
 	</div>
 
 	<div class="row panel radius" id="Susuan" style="margin-left: auto; margin-right: auto; margin-bottom:22px;">
-		<div class="ten columns callout panel">
-				Something about Susan
-		</div>
 		<div class="two columns">
-				<img src="http://placehold.it/400x300&text=[img]">
+			<img src="http://placehold.it/400x300&text=[img]">
+		</div>
+		<div class="three columns callout panel" style="margin: 0 auto 0 auto; text-align:center;">
+			<h3>Susan<h3>
+		</div>
+		<div class="seven columns">
+				Something about Susan
 		</div>
 	</div>
 
@@ -30,15 +30,16 @@
 		<div class="two columns">
 				<img src="/project/images/tom.jpeg">
 		</div>
-		<div class="ten columns callout panel">
-				On web development, interface design, user research, and all the rest.
-				I research HCI and computing education at Drexel University in Philadelphia.
-				<ul>
-			<li style="margin-right:10px;"><a class=".fc-webicon facebook" href="http://twitter.com/thomashpark" title="Twitter"><strong>Twitter</strong></a></li>
-			<!-- <li class="git" style="margin-right:10px;"><a href="http://github.com/thomaspark" title="GitHub"><strong>GitHub</strong></a></li>
-			<li class="rss" style="margin-right:10px;"><a href="http://feeds.feedburner.com/thomaspark" title="RSS Feed"><strong>RSS</strong></a></li> -->
-			
-		</ul>
+		<div class="three columns callout panel" style="margin: 0 auto 0 auto; text-align:center;">
+			<h4>Thomas Park</h4>
+			<a class="fc-webicon twitter" href="http://twitter.com/thomashpark" title="Twitter"><strong>Twitter</strong></a>
+			<a class="fc-webicon github" href="http://github.com/thomaspark" title="GitHub"><strong>GitHub</strong></a>
+			<a class="fc-webicon rss" href="http://feeds.feedburner.com/thomaspark" title="RSS Feed"><strong>RSS</strong></a>
+
+		</div>
+		<div class="seven columns">
+			On web development, interface design, user research, and all the rest.
+			I research HCI and computing education at Drexel University in Philadelphia.
 		</div>
 	</div>
 
@@ -48,17 +49,23 @@
 		<div class="two columns">
 				<img src="http://placehold.it/400x300&text=[img]">
 		</div>
-		<div class="ten columns callout panel">
+		<div class="three columns callout panel" style="margin: 0 auto 0 auto; text-align:center;">
+			<h3>Swathi<h3>
+		</div>
+		<div class="seven columns">
 				Something about Swathi here.
 		</div>
 	</div>
 
 	<div class="row panel radius" id="Ankur" style="margin-left: auto; margin-right: auto; margin-bottom:22px;">
-		<div class="ten columns callout panel">
-				Something about myself.
-		</div>
 		<div class="two columns">
 				<img src="/project/images/ankur.png">
 		</div>
+		<div class="three columns callout panel" style="margin: 0 auto 0 auto; text-align:center;">
+			<h4>Ankur Saxena</h4>
+		</div>
+		<div class="seven columns">
+				Something about myself.
+		</div>		
 	</div>
 
