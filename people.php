82f2597aa9b3f9d0bfb0400df90191ccf9683c13
Ankur <as3445@drexel.edu>
2013-01-09 16:33:26 -0500

    formatted people's page

--- people.php
+++ people.php
@@ -5,8 +5,9 @@
 		<div class="two columns">
 				<img src="/project/images/andrea.jpg">
 		</div>
-		<div class="three columns callout panel" style="margin: 0 auto 0 auto; text-align:center;">
-			<h3>Andrea Forte<h3>
+		<div class="three columns button" style="margin: 0 auto 0 auto; text-align:left; height: 120px;">
+			<h4>Andrea Forte</h4>
+			andrea.forte@drexel.edu
 		</div>
 		<div class="seven columns">
 			Dr. Forte is interested in understanding new forms of information production and sharing that are made possible by participatory media.
@@ -16,10 +17,11 @@
 
 	<div class="row panel radius" id="Susuan" style="margin-left: auto; margin-right: auto; margin-bottom:22px;">
 		<div class="two columns">
-			<img src="http://placehold.it/400x300&text=[img]">
+			<img src="http://placehold.it/120x120&text=[img]">
 		</div>
-		<div class="three columns callout panel" style="margin: 0 auto 0 auto; text-align:center;">
-			<h3>Susan<h3>
+		<div class="three columns button" style="margin: 0 auto 0 auto; text-align:left; height: 120px;">
+			<h4>Susan Won</h4>
+			susan.won@drexel.edu
 		</div>
 		<div class="seven columns">
 				Something about Susan
@@ -30,11 +32,10 @@
 		<div class="two columns">
 				<img src="/project/images/tom.jpeg">
 		</div>
-		<div class="three columns callout panel" style="margin: 0 auto 0 auto; text-align:center;">
+		<div class="three columns button" style="margin: 0 auto 0 auto; text-align:left; height: 120px;">
 			<h4>Thomas Park</h4>
-			<a class="fc-webicon twitter" href="http://twitter.com/thomashpark" title="Twitter"><strong>Twitter</strong></a>
-			<a class="fc-webicon github" href="http://github.com/thomaspark" title="GitHub"><strong>GitHub</strong></a>
-			<a class="fc-webicon rss" href="http://feeds.feedburner.com/thomaspark" title="RSS Feed"><strong>RSS</strong></a>
+			thomas.park@drexel.edu
+
 
 		</div>
 		<div class="seven columns">
@@ -47,10 +48,11 @@
 
 	<div class="row panel radius" id="Swathi" style="margin-left: auto; margin-right: auto; margin-bottom:22px;">
 		<div class="two columns">
-				<img src="http://placehold.it/400x300&text=[img]">
+				<img src="http://placehold.it/120x120&text=[img]">
 		</div>
-		<div class="three columns callout panel" style="margin: 0 auto 0 auto; text-align:center;">
-			<h3>Swathi<h3>
+		<div class="three columns button" style="margin: 0 auto 0 auto; text-align:left; height: 120px;">
+			<h5>Swathi Jagannath</h5>
+				swathi.jagannath@drexel.edu
 		</div>
 		<div class="seven columns">
 				Something about Swathi here.
@@ -61,8 +63,9 @@
 		<div class="two columns">
 				<img src="/project/images/ankur.png">
 		</div>
-		<div class="three columns callout panel" style="margin: 0 auto 0 auto; text-align:center;">
+		<div class="three columns button" style="margin: 0 auto 0 auto; text-align:left; height: 120px;">
 			<h4>Ankur Saxena</h4>
+			ankur.saxena@drexel.edu
 		</div>
 		<div class="seven columns">
 				Something about myself.
